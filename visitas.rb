0678b90c628d1e668555354c15ed1143d00a53c1
Ale Fuentes <jano231982@gmail.com>
2020-11-04 16:11:33 -0300

    visitas ok

---
+++ visitas.rb
@@ -0,0 +1,17 @@
+
+def promedio(arr)
+cant = arr.length
+suma = 0  
+
+arr.each do |elemento|
+  suma += elemento.to_i  
+end  
+
+suma / cant 
+
+end
+
+
+visitas = [1000, 800, 250, 300, 500, 2500]
+
+print promedio(visitas)
